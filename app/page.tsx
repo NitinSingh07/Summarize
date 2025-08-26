2c83bcef1126510131838255279425b1ae0d77cc
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-08-26 17:04:50 +0530

    Integrate PricingSection and CTASection into Home page, enhancing the layout with dynamic pricing plans.

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       </div>
       <DemoSection />
       <HowItWorkSection/>
-      {/* <PricingSection/> */}
-      {/* <CTASection/> */}
+      <PricingSection/>
+      <CTASection/>
     </div>
   );
 }
