c5fa74f84c3648be401ffacf8e827f57b02d25cd
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-08-28 19:41:17 +0530

    new UploadHeader

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -1,6 +1,6 @@
 import BgGradient from "@/components/common/bggradient";
-import { Badge } from "@/components/ui/badge";
-import { Sparkles } from "lucide-react";
+import Uploadform from "@/components/upload/upload-form";
+import UploadHeader from "@/components/upload/upload-header";
 
 export default function Page() {
   return (
@@ -8,28 +8,8 @@ export default function Page() {
       <BgGradient />
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
-            <Badge
-              variant={"secondary"}
-              className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-gray-50 transition-colors"
-            >
-              <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse" />
-              <span>AI-Powered Content Creation</span>
-            </Badge>
-          </div>
-          <div className="capitalize text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Start Uploading{" "}
-            <span className="relative inline-block">
-              <span className="relative z-10 px-2">Your PDF's</span>
-              <span
-                className="absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1"
-                aria-hidden="true"
-              ></span>
-            </span>{" "}
-          </div>
-          <p className="text-lg leading-8 text-gray-600">
-            Upload your PDF and let our AI do the magic! ✨
-          </p>
+          <UploadHeader />
+          <Uploadform />
         </div>
       </div>
     </section>
